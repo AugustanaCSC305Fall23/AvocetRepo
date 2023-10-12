71336f503a53fa4158c8097dec7b6249602ca7ee
Jay Nolledo <jaynolleds19@gmail.com>
2023-10-11 21:50:02 -0500

    Added Blank Lesson Plan

--- AvocetProject/src/main/java/edu/augustana/NewLessonPlanController.java
+++ AvocetProject/src/main/java/edu/augustana/NewLessonPlanController.java
@@ -3,6 +3,7 @@ package edu.augustana;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 
 public class NewLessonPlanController {
 
@@ -11,10 +12,17 @@ public class NewLessonPlanController {
 
     @FXML
     private URL location;
-
+    @FXML
+    private ComboBox<String> eventComboBox;
+    @FXML
+    private ComboBox<String> keyWordsComboBox;
     @FXML
     void initialize() {
+        eventComboBox.getItems().addAll("Floor", "Bar", "Mushroom", "Rings");
+        keyWordsComboBox.getItems().addAll("Floor", "Bar", "Mushroom", "Rings", "KeyWord1", "KeyWord2");
 
     }
+
+
 
 }
